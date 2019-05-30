5b6ced59439921969255a932ed5a6d47e581e2b9
Khang <nghiemkhang7598@gmail.com>
2019-05-30 22:07:50 +0700

    First Commit

---
+++ JapaneWebsite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using JapaneWebsite;
+using JapaneWebsite.Models;
+
+namespace JapaneWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private JapaneDataEntities db = new JapaneDataEntities();
+        // GET: Home
+        public List<StudyPost> GetStudyPost()
+        {
+            return db.StudyPosts.ToList();
+        }
+        public List<ThemeOfPost> GetThemeOfPosts()
+        {
+            return db.ThemeOfPosts.ToList();
+        }
+        public List<CulturalPost> GetCulturalPosts()
+        {
+            return db.CulturalPosts.ToList();
+        }
+
+        public ActionResult Index()
+        {
+            ViewModel model = new ViewModel();
+            model.studyPosts = GetStudyPost();
+            model.themeOfPosts = GetThemeOfPosts();
+            model.culturalPosts = GetCulturalPosts();
+            return View(model);
+        }
+    }
+}
