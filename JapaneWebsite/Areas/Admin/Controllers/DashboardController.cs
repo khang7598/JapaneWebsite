e23f33f1b821ba66729b53b488a1369eb0f67bd4
HimatsuT <nhattung0205@gmail.com>
2019-06-18 16:28:59 +0700

    Update

--- JapaneWebsite/Areas/Admin/Controllers/DashboardController.cs
+++ JapaneWebsite/Areas/Admin/Controllers/DashboardController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace JapaneWebsite.Areas.Admin.Controllers
 {
+    [Authorize(Roles = "MANAGER,ADMIN")]
     public class DashboardController : Controller
     {
         // GET: Admin/Dashboard
